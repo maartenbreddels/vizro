8556ae5fbe0b039f52059837ef4a22b0684daac8
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-05-01 10:00:26 +0200

    Add alternative homepage design

--- vizro-core/examples/_dev/app.py
+++ vizro-core/examples/_dev/app.py
@@ -1,137 +1,155 @@
 """Rough example used by developers."""
 
-from typing import Literal
-
 import vizro.models as vm
 import vizro.plotly.express as px
 from vizro import Vizro
-from vizro.models._components.form._text_area import TextArea
-from vizro.models._components.form._user_input import UserInput
 
 iris = px.data.iris()
-
-
-class CustomInput(UserInput):
-    """Custom Input that allows passing `className` as an argument."""
-
-    type: Literal["other_input"] = "other_input"
-    class_name: str = "form-control"
-
-    def build(self):
-        """Build the component."""
-        input_build_obj = super().build()
-        input_build_obj[self.id].className = self.class_name
-        return input_build_obj
-
-
-# Only added to container.components directly for dev example
-vm.Page.add_type("controls", UserInput)
-vm.Page.add_type("controls", TextArea)
-vm.Container.add_type("components", vm.Dropdown)
-vm.Container.add_type("components", vm.Checklist)
-vm.Container.add_type("components", vm.RadioItems)
-vm.Container.add_type("components", vm.Slider)
-vm.Container.add_type("components", vm.RangeSlider)
-vm.Container.add_type("components", TextArea)
-vm.Container.add_type("components", UserInput)
-vm.Container.add_type("components", CustomInput)
-
-selectors = vm.Page(
-    title="Selectors - Controls",
-    components=[
-        vm.Graph(
-            id="scatter_relation",
-            figure=px.scatter(data_frame=px.data.gapminder(), x="gdpPercap", y="lifeExp", size="pop"),
-        ),
-        vm.Card(
-            text="""
-                # This is an <h1> tag
-                ## This is an <h2> tag
-                ###### This is an <h6> tag
-                \n
-                >
-                > Block quotes are used to highlight text.
-                >
-                \n
-                * Item 1
-                * Item 2
-                \n
-                 *This text will be italic*
-                _This will also be italic_
-                **This text will be bold**
-                _You **can** combine them_
-            """,
-        ),
-    ],
-    controls=[
-        vm.Filter(
-            column="continent",
-            selector=vm.Dropdown(title="Dropdown Label"),
-        ),
-        vm.Filter(
-            column="year",
-            selector=vm.RangeSlider(title="Range Slider Label", step=1, marks=None),
-        ),
-        vm.Filter(
-            column="year",
-            selector=vm.Slider(title="Slider Label"),
-        ),
-        vm.Filter(
-            column="year",
-            selector=vm.RangeSlider(title="Range Slider Label", step=10),
-        ),
-        vm.Filter(
-            column="year",
-            selector=vm.Slider(title="Slider Label", step=10),
-        ),
-        vm.Filter(
-            column="continent",
-            selector=vm.Checklist(title="Checklist Label"),
-        ),
-        vm.Filter(
-            column="continent",
-            selector=vm.RadioItems(title="Radio Items Label"),
-        ),
-        UserInput(title="Input - Text (single-line)", placeholder="Enter text here"),
-        TextArea(title="Input - Text (multi-line)", placeholder="Enter multi-line text here"),
-    ],
-)
-
-form_components = vm.Page(
-    title="Selectors - Components",
+page_home = vm.Page(
+    title="Homepage",
+    description="Vizro demo app for studying gapminder data",
+    layout=vm.Layout(grid=[[0], [1], [2]], col_gap="80px", row_gap="80px", row_min_height="372px"),
     components=[
         vm.Container(
-            id="container-id",
-            title="Form",
+            title="Group 1",
+            layout=vm.Layout(grid=[[0, 1, 2, 3]], col_gap="40px", row_gap="40px"),
             components=[
-                UserInput(title="Input - Text (single-line)", placeholder="Enter text here"),
-                CustomInput(
-                    title="Input - Text (single-line)",
-                    placeholder="Enter text here",
-                    class_name="form-control is-valid",
+                vm.Card(
+                    text="""
+                       ![](assets/images/icons/hypotheses.svg#icon-top)
+            
+                       ### Variable Analysis
+            
+                       Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+                       labore et dolore magna aliquyam erat, sed diam voluptua.
+            
+                       ![](assets/images/screens/page-1.png#screen-img)
+                   """,
+                    href="/page-1",
                 ),
-                CustomInput(
-                    title="Input - Text (single-line)",
-                    placeholder="Enter text here",
-                    class_name="form-control is-invalid",
+                vm.Card(
+                    text="""
+               ![](assets/images/icons/hypotheses.svg#icon-top)
+
+               ### Relationship Analysis
+
+               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+               labore et dolore magna aliquyam erat, sed diam voluptua.
+
+               ![](assets/images/screens/page-2.png#screen-img)
+           """,
+                    href="/page-2",
                 ),
-                TextArea(title="Input - Text (multi-line)", placeholder="Enter multi-line text here"),
-                vm.Dropdown(title="Dropdown - Single", options=["Option 1", "Option 2", "Option 3"], multi=False),
-                vm.Dropdown(title="Dropdown - Multi", options=["Option 1", "Option 2", "Option 3"], multi=True),
-                vm.Checklist(title="Checklist", options=["Option 1", "Option 2", "Option 3"]),
-                vm.RadioItems(title="Radio Items", options=["Option 1", "Option 2", "Option 3"]),
-                vm.Slider(title="Slider without marks", min=0, max=10),
-                vm.Slider(title="Slider with marks", min=0, max=10, step=1),
-                vm.RangeSlider(title="Range Slider without marks", min=0, max=10),
-                vm.RangeSlider(title="Range Slider with marks", min=0, max=10, step=1),
-                vm.Button(),
+                vm.Card(
+                    text="""
+                   ![](assets/images/icons/collections.svg#icon-top)
+
+                   ### Continent Summary
+
+                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+                   labore et dolore magna aliquyam erat, sed diam voluptua.
+
+                   ![](assets/images/screens/page-3.png#screen-img)
+               """,
+                    href="/page-3",
+                ),
+                vm.Card(
+                    text="""
+                    ![](assets/images/icons/collections.svg#icon-top)
+                
+                    ### Continent Summary
+                
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+                    labore et dolore magna aliquyam erat, sed diam voluptua.
+                
+                    ![](assets/images/screens/page-3.png#screen-img)
+                """,
+                    href="/page-4",
+                ),
+            ],
+        ),
+        vm.Container(
+            title="Group 2",
+            layout=vm.Layout(grid=[[0, 1, -1, -1]], col_gap="40px", row_gap="40px"),
+            components=[
+                vm.Card(
+                    text="""
+                   ![](assets/images/icons/features.svg#icon-top)
+
+                   ### Benchmark Analysis
+
+                   Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+                   labore et dolore magna aliquyam erat, sed diam voluptua.
+
+                   ![](assets/images/screens/page-4.png#screen-img)
+               """,
+                    href="/page-4",
+                ),
+                vm.Card(
+                    text="""
+               ![](assets/images/icons/hypotheses.svg#icon-top)
+
+               ### Other Analysis
+
+               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+               labore et dolore magna aliquyam erat, sed diam voluptua.
+
+               ![](assets/images/screens/page-1.png#screen-img)
+           """,
+                    href="/page-5",
+                ),
+            ],
+        ),
+        vm.Container(
+            title="Group 3",
+            layout=vm.Layout(grid=[[0, 1, 2, -1]], col_gap="40px", row_gap="40px"),
+            components=[
+                vm.Card(
+                    text="""
+    ![](assets/images/icons/hypotheses.svg#icon-top)
+
+    ### Variable Analysis
+
+    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+    labore et dolore magna aliquyam erat, sed diam voluptua.
+
+    ![](assets/images/screens/page-1.png#screen-img)
+""",
+                    href="/page-1",
+                ),
+                vm.Card(
+                    text="""
+        ![](assets/images/icons/hypotheses.svg#icon-top)
+
+        ### Relationship Analysis
+
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+        labore et dolore magna aliquyam erat, sed diam voluptua.
+
+        ![](assets/images/screens/page-2.png#screen-img)
+    """,
+                    href="/page-2",
+                ),
+                vm.Card(
+                    text="""
+            ![](assets/images/icons/collections.svg#icon-top)
+
+            ### Continent Summary
+
+           Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut 
+            labore et dolore magna aliquyam erat, sed diam voluptua.
+
+            ![](assets/images/screens/page-3.png#screen-img)
+        """,
+                    href="/page-3",
+                ),
             ],
         ),
     ],
 )
 
-
-dashboard = vm.Dashboard(pages=[selectors, form_components])
+page_one = vm.Page(title="Page 1", components=[vm.Card(text="""Placeholder""")])
+dashboard = vm.Dashboard(pages=[page_home, page_one], title="Demo")
 
 if __name__ == "__main__":
     Vizro().build(dashboard).run()
