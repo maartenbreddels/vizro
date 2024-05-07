2e673f8711ce782df0767714fe8832a4f734e2c1
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-05-07 12:22:23 +0200

    Finalized POC

--- vizro-ai/examples/dashboard/plan.py
+++ vizro-ai/examples/dashboard/plan.py
@@ -9,6 +9,8 @@ from langchain_anthropic import ChatAnthropic
 import vizro.models as vm
 from vizro.tables import dash_ag_grid
 from vizro.models.types import SelectorType
+from vizro.managers import model_manager
+from emoji import emojize
 
 component_type = Literal["AgGrid", "Card", "Graph"]
 control_type = Literal["Filter"]
@@ -17,7 +19,7 @@ control_type = Literal["Filter"]
 class Component(BaseModelV1):
     component_name: component_type
     component_description: str = Field(...,
-                                       description="Description of the component. Include everything that seems to relate to this component.")
+                                       description="Description of the component. Include everything that seems to relate to this component. If possible do not paraphrase and keep the original description. Keep any links as original links.")
     component_id: str = Field(pattern=r'^[a-z]+(_[a-z]+)?$',
                               description="Small snake case description of this component")
 
@@ -33,6 +35,7 @@ class Component(BaseModelV1):
 class Components(BaseModelV1):
     components: List[Component]
 
+
 def create_filter_proxy(df, available_components):
     def validate_targets(v):
         if v not in available_components:
@@ -43,16 +46,20 @@ def create_filter_proxy(df, available_components):
         if v not in df.columns:
             raise ValueError(f"column must be one of {list(df.columns)}")
         return v
-    #TODO: properly check this - e.g. what is the best way to ideally dynamically include the available components
+
+    # TODO: properly check this - e.g. what is the best way to ideally dynamically include the available components
     # even in the schema
     return create_model(
-    'FilterProxy',
-    column=(str, Field(...,description="The target column of the filter.")),
-    targets=(List[str], Field([],description="The targets of the filter. Listen carefully to the instructions here.")),
-    __validators__={"validator1": validator("targets", pre=True,each_item=True,allow_reuse=True)(validate_targets),
-                    "validator2": validator("column",allow_reuse=True)(validate_column)},
-    __base__=vm.Filter,
-)
+        'FilterProxy',
+        column=(str, Field(..., description="The target column of the filter.")),
+        targets=(
+        List[str], Field([], description="The targets of the filter. Listen carefully to the instructions here.")),
+        __validators__={
+            "validator1": validator("targets", pre=True, each_item=True, allow_reuse=True)(validate_targets),
+            "validator2": validator("column", allow_reuse=True)(validate_column)},
+        __base__=vm.Filter,
+    )
+
 
 class Control(BaseModelV1):
     control_name: control_type
@@ -63,9 +70,11 @@ class Control(BaseModelV1):
     def create(self, df, model, available_components):
         filter_prompt = (
             f"Create a filter from the following instructions: {self.control_description}. Do not make up "
-            f"things that are optional and do not configure actions. If no options are specified, leave them out.")
-        proxy = get_model(filter_prompt, model, result_model=create_filter_proxy(df,available_components))
-        return vm.Filter.parse_obj(proxy.dict(exclude={'selector': {'id':True}, 'id': True, 'type':True}))
+            f"things that are optional and DO NOT configure actions, action triggers or action chains. If no options are specified, leave them out.")
+        proxy = get_model(filter_prompt, model, result_model=create_filter_proxy(df, available_components))
+        actual = vm.Filter.parse_obj(proxy.dict(exclude={'selector': {'id': True, 'actions':True}, 'id': True, 'type': True}))
+        del model_manager._ModelManager__models[proxy.id] # TODO: This is very wrong and needs to change
+        return actual
 
 
 class Controls(BaseModelV1):
@@ -80,12 +89,29 @@ class PagePlanner(BaseModelV1):
 
 
 class DashboardPlanner(BaseModelV1):
+    title: str = Field(...,
+                       description="Title of the dashboard. If no description is provided, make a short and concise title from the content of the pages.")
     pages: List[PagePlanner]
 
 
 def get_dashboard_plan(query: str, model: Union[ChatOpenAI, ChatAnthropic],
                        max_retry: int = 3) -> PagePlanner:
     return get_model(query, model, result_model=DashboardPlanner, max_retry=max_retry)
+
+def print_dashboard_plan(dashboard_plan):
+    for i, page in enumerate(dashboard_plan.pages):
+        print(
+            f"{emojize(':page_facing_up::page_facing_up:')} " + '\033[1m' + f" PAGE: {page.title} " + '\033[0m' + f"{emojize(':page_facing_up::page_facing_up:')} ")
+        print(emojize(':package::package::package::package:  COMPONENTS :package::package::package::package:',
+                      language='alias'))
+        for j in page.components.components:
+            print("--> "+repr(j))
+        # available_components = [comp.component_id for comp in dashboard_plan.pages[0].components.components]
+        print(emojize(':control_knobs::control_knobs::control_knobs::control_knobs:  CONTROLS :control_knobs::control_knobs::control_knobs::control_knobs:',
+                      language='alias'))
+        for j in page.controls.controls:
+            print("--> "+repr(j))
+        print("\n")
 
 
 if __name__ == "__main__":
@@ -109,30 +135,12 @@ if __name__ == "__main__":
     # model = ChatAnthropic(
     #     model='claude-3-opus-20240229',
     #     anthropic_api_key=os.environ.get("ANTHROPIC_API_KEY"),
-    #     anthropic_api_url=os.environ.get("ANTHROPIC_BASE_URL")
+    #     anthropic_api_url=os.environ.get("ANTHROPIC_API_BASE")
     # )
 
     fig_builder = VizroAI(model=model)
     gapminder = px.data.gapminder()
 
     plan = get_dashboard_plan(query, model)
-    for i in plan.pages[0].components.components:
-        print(repr(i))
-    available_components = [comp.component_id for comp in plan.pages[0].components.components]
-    for i in plan.pages[0].controls.controls:
-        print(repr(i))
 
-    page = vm.Page(
-        title="My first dashboard",
-        components=[
-            comp.create(fig_builder=fig_builder, df=gapminder) for comp in plan.pages[0].components.components
-        ],
-        controls=[
-            contr.create(df=gapminder, available_components=available_components) for contr in
-            plan.pages[0].controls.controls
-        ],
-    )
-
-    dashboard = vm.Dashboard(pages=[page])
-
-    Vizro().build(dashboard).run()
+    print_dashboard_plan(plan)
--- vizro-ai/examples/ex_1.py
+++ vizro-ai/examples/ex_1.py
@@ -7,7 +7,8 @@ from vizro_ai import VizroAI
 from vizro import Vizro
 import plotly.express as px
 from dashboard.model import get_model
-from dashboard.plan import get_dashboard_plan
+from dashboard.plan import get_dashboard_plan, print_dashboard_plan
+from dashboard.build import DashboardBuilder
 
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
@@ -17,14 +18,8 @@ import sys
 
 sys.path.append('./dashboard')
 
-query = ("I need a page with a bar chart shoing the population per continent "
-         "and a scatter chart showing the life expectency per country as a function gdp. "
-         "Make a filter on the continent column. This filter should only "
-         "apply to the bar chart. Make another filter that filter the scatter chart for population. "
-         "The bar chart should be a stacked bar chart, while "
-         "the scatter chart should be colored by the column 'continent'. "
-         " I also want a table that shows the data. The title of the page should be `My wonderful "
-         "jolly dashboard showing a lot of data`.")
+from dotenv import load_dotenv
+
 
 model = ChatOpenAI(model="gpt-4-turbo", temperature=0)
 # model = ChatOpenAI(model="gpt-4-turbo", temperature=0)
@@ -34,26 +29,23 @@ model = ChatOpenAI(model="gpt-4-turbo", temperature=0)
 #     anthropic_api_key=os.environ.get("ANTHROPIC_API_KEY"),
 #     anthropic_api_url=os.environ.get("ANTHROPIC_BASE_URL")
 # )
-
 fig_builder = VizroAI(model=model)
-gapminder = px.data.gapminder()
 
-plan = get_dashboard_plan(query, model)
-for i in plan.pages[0].components.components:
-    print(repr(i))
-available_components = [comp.component_id for comp in plan.pages[0].components.components]
-for i in plan.pages[0].controls.controls:
-    print(repr(i))
+# df = px.data.gapminder()
+# query = ("I need a page with a bar chart shoing the population per continent "
+#          "and a scatter chart showing the life expectency per country as a function gdp. "
+#          "Make a filter on the GDP column and use a dropdown as selector. This filter should only "
+#          "apply to the bar chart. The bar chart should be a stacked bar chart, while "
+#          "the scatter chart should be colored by the column 'continent'. I also want "
+#          "a table that shows the data. The title of the page should be: `This is big time data`. I also want a second page with just "
+#          "a card on it that links to `https://vizro.readthedocs.io/`. The title of the dashboard should be: `My wonderful "
+#          "jolly dashboard showing a lot of data`.")
+df = px.data.tips()
+query = ("I want to create a Vizro style dashboard. I need One page. On this page, there are two charts. The first bar chart shows the average tip per day. The second chart shows the distribution of total bill. Add a checklist filter to filter on the time. On this page, also add a card with text 'This page is using the tips dataset.' ")
 
-page = vm.Page(
-    title=plan.pages[0].title,
-    components=[
-        comp.create(fig_builder=fig_builder, df=gapminder, model=model) for comp in plan.pages[0].components.components
-    ],
-    controls=[y for x in plan.pages[0].controls.controls if
-              (y := x.create(df=gapminder, model=model, available_components=available_components)) is not None],
-)
+dashboard_plan = get_dashboard_plan(query, model)
+print_dashboard_plan(dashboard_plan)
 
-dashboard = vm.Dashboard(pages=[page])
+dashboard = DashboardBuilder(model=model, data=df, dashboard_plan=dashboard_plan, fig_builder=fig_builder).dashboard
 
 Vizro().build(dashboard).run()
