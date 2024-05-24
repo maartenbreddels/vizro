4d01dfabdd12662c15b172fa005d9478349ff881
Maximilian Schulz <maximilian_schulz@mckinsey.com>
2024-05-24 10:06:52 +0200

    Trying out pydantic to python

--- vizro-ai/examples/example.py
+++ vizro-ai/examples/example.py
@@ -3,7 +3,10 @@
 import plotly.express as px
 from vizro_ai import VizroAI
 
-vizro_ai = VizroAI()
+from dotenv import load_dotenv
+load_dotenv()
+
+vizro_ai = VizroAI(model="gpt-4-turbo")
 df = px.data.gapminder()
-fig = vizro_ai.plot(df, "describe the composition of gdp in continent,and horizontal line for avg gdp")
+fig = vizro_ai.plot(df, "describe the composition of gdp in continent,and horizontal line for avg gdp", explain=True)
 fig.show()
