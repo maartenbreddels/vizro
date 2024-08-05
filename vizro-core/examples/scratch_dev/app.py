90128df6d192f158ce352302214cae07dff29056
huong-li-nguyen <huong_li_nguyen@mckinsey.com>
2024-08-05 15:54:15 +0200

    Add example code

--- vizro-core/examples/scratch_dev/app.py
+++ vizro-core/examples/scratch_dev/app.py
@@ -1,111 +1,112 @@
 """Dev app to try things out."""
 
+import random
+import pandas as pd
+
 import vizro.models as vm
 import vizro.plotly.express as px
+
 from vizro import Vizro
-from vizro.figures import kpi_card
 from vizro.models.types import capture
-from vizro.tables import dash_ag_grid, dash_data_table
-
-df = px.data.iris()
 
 
-# Graph
+# Load the data
+df = pd.read_csv("linkedin_input_data.csv")
+
+# Clean the data
+df.dropna(subset=["Position", "Company"], inplace=True)
+df['Connected On'] = pd.to_datetime(df['Connected On'], format='%d-%b-%y')
+df.set_index('Connected On', drop=False, inplace=True)
+df['Year'] = df['Connected On'].dt.year
+
+icons = ["👨", "👩", "👧", "👦", "🧒"]
+
+
 @capture("graph")
-def my_graph_figure(data_frame, **kwargs):
-    """My custom figure."""
-    return px.scatter(data_frame, **kwargs)
+def role_chart(data_frame, top_n=5):
+    # Sample data preparation
+    data_frame = data_frame['Position'].value_counts().nlargest(top_n).reset_index(name="Frequency")
+    data_frame = data_frame.sort_values(by='Frequency', ascending=True)
+
+    # Create a new DataFrame to hold the dots
+    dot_list = []
+    for i, row in data_frame.iterrows():
+        dot_list.append(pd.DataFrame({
+            'Position': [row['Position']] * row['Frequency'],
+            'Frequency': list(range(1, row['Frequency'] + 1)),
+            'Icon': [random.choice(icons) for _ in range(row['Frequency'])]
+        }))
+    dots_df = pd.concat(dot_list, ignore_index=True)
+
+    fig = px.scatter(dots_df, x='Frequency', y='Position', title=f'Top {top_n} most frequent positions', text="Icon")
+    fig.update_layout(yaxis=dict(showgrid=False))
+
+    return fig
 
 
-class MyGraph(vm.Graph):
-    """My custom class."""
+@capture("graph")
+def company_chart(data_frame, top_n=10):
+    # Plot the most common companies among my connections
+    data_frame = data_frame['Company'].value_counts().nlargest(top_n).reset_index(name="Count")
 
-    def build(self):
-        """Custom build."""
-        graph_build_obj = super().build()
-        # DO SOMETHING
-        return graph_build_obj
+    fig = px.pie(data_frame, names='Company', values='Count', title=f'Top {top_n} most connected companies', hole=0.6)
+    fig.update_layout(legend=dict(x=1, y=1, orientation='v'))
 
-
-# Table
-@capture("table")
-def my_table_figure(data_frame, **kwargs):
-    """My custom figure."""
-    return dash_data_table(data_frame, **kwargs)()
+    return fig
 
 
-class MyTable(vm.Table):
-    """My custom class."""
+@capture("graph")
+def growth_cumulative_chart(data_frame):
+    # Plot the growth of my connections over time
+    data_frame = data_frame.groupby('Year').size().reset_index(name="Count per Year")
+    data_frame['Cumulative Yearly Connections'] = data_frame['Count per Year'].cumsum()
 
-    pass
+    line_fig = px.line(data_frame, x="Year", y='Cumulative Yearly Connections', color=px.Constant("Cumulative Connections"))
+    line_fig.update_traces(yaxis='y2', line_color='#F08D41')
 
+    bar_fig = px.bar(data_frame, x='Year', y='Count per Year', color=px.Constant("Count per Year"), title='Yearly growth of my connections')
+    bar_fig.add_traces(line_fig.data)
 
-# AgGrid
-@capture("ag_grid")
-def my_ag_grid_figure(data_frame, **kwargs):
-    """My custom figure."""
-    return dash_ag_grid(data_frame, **kwargs)()
+    bar_fig.update_layout(
+        yaxis=dict(title='Per Year Connections [Bar]'),
+        yaxis2=dict(title='Cumulative Connections [Line]', overlaying='y', side='right'),
+        legend=dict(title="", yanchor="bottom", y=1, xanchor="right", x=1),
+    )
 
-
-class MyAgGrid(vm.AgGrid):
-    """My custom class."""
-
-    pass
-
-
-# Figure
-@capture("figure")
-def my_kpi_card_figure(data_frame, **kwargs):
-    """My custom figure."""
-    return kpi_card(data_frame, **kwargs)()
-
-
-class MyFigure(vm.Figure):
-    """My custom class."""
-
-    pass
-
-
-# Action
-@capture("action")
-def my_action_function():
-    """My custom action."""
-    pass
-
-
-class MyAction(vm.Action):
-    """My custom class."""
-
-    pass
+    return bar_fig
 
 
 page = vm.Page(
-    title="Test",
-    layout=vm.Layout(
-        grid=[[0, 1], [2, 3], [4, 5], [6, 7], [8, -1]],
-        col_gap="50px",
-        row_gap="50px",
-    ),
+    title="LinkedIn data",
+    layout=vm.Layout(grid=[
+        [0, 1],
+        [0, 2]
+    ]),
     components=[
-        # Graph
-        MyGraph(figure=px.scatter(df, x="sepal_width", y="sepal_length", title="My Graph")),
-        MyGraph(figure=my_graph_figure(df, x="sepal_width", y="sepal_length", title="My Graph Custom Figure")),
-        # Table
-        MyTable(figure=dash_data_table(df), title="My Table"),
-        MyTable(figure=my_table_figure(df), title="My Table Custom Figure"),
-        # AgGrid
-        MyAgGrid(figure=dash_ag_grid(df), title="My AgGrid"),
-        MyAgGrid(figure=my_ag_grid_figure(df), title="My AgGrid Custom Figure"),
-        # Figure
-        MyFigure(figure=kpi_card(df, value_column="sepal_width", title="KPI Card")),
-        MyFigure(figure=my_kpi_card_figure(df, value_column="sepal_width", title="KPI Card Custom Figure")),
-        # Action
-        MyGraph(
-            figure=my_graph_figure(df, x="sepal_width", y="sepal_length", title="My Graph Custom Figure"),
-            actions=[MyAction(function=my_action_function())],
-        ),
+        vm.Graph(id="role_chart_id", figure=role_chart(data_frame=df)),
+        vm.Graph(id="company_chart_id", figure=company_chart(data_frame=df)),
+        vm.Graph(figure=growth_cumulative_chart(data_frame=df)),
     ],
-    controls=[vm.Filter(column="species")],
+    controls=[
+        vm.Filter(
+            column="Position",
+            selector=vm.Dropdown(
+                options=df['Position'].value_counts().loc[lambda x: x >= 5].index.tolist()
+            )
+        ),
+        vm.Filter(column="Company"),
+        vm.Parameter(
+            targets=["role_chart_id.top_n"],
+            selector=vm.Slider(min=1, max=30, step=1, marks={1: "1", 10: "10", 20: "20", 30: "30"}, value=30,
+                               title="Top N roles")
+        ),
+        vm.Parameter(
+            targets=["company_chart_id.top_n"],
+            selector=vm.Slider(min=1, max=20, step=1, marks={1: "1", 5: "5", 10: "10", 15: "15", 20: "20"}, value=5,
+                               title="Top N companies")
+        ),
+        vm.Filter(column="Year", selector=vm.RangeSlider(step=1, marks=None)),
+    ],
 )
 
 dashboard = vm.Dashboard(pages=[page])
